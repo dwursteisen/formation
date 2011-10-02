ce301cbd5675aa501307585758ae92bb09d4c402
dwursteisen <david.wursteisen+git@gmail.com>
2011-10-02 23:48:49 +0200

    recherche d'implementation par reflexion

---
+++ 02 - programmation par contrat/test/my/test/MonProgrammeTest.java
@@ -0,0 +1,23 @@
+package my.test;
+
+import my.test.implementation.MonImprementationDeBase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: david
+ * Date: 02/10/11
+ * Time: 23:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class MonProgrammeTest {
+    @Test
+    public void testConstructionAnnuaire() throws Exception {
+        MonAnnuaire instance = MonProgramme.constructionAnnuaire();
+        assertNotNull(instance);
+        assertTrue(MonImprementationDeBase.class.isInstance(instance));
+    }
+}
