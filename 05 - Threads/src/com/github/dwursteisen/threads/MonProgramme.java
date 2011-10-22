c0b7a00369f00559d7e52a8b964ccd5150b16463
dwursteisen <david.wursteisen+git@gmail.com>
2011-10-23 00:31:51 +0200

    création d'un thread qui interrupt les threads et qui vérifie le soft après

--- 05 - Threads/src/com/github/dwursteisen/threads/MonProgramme.java
+++ 05 - Threads/src/com/github/dwursteisen/threads/MonProgramme.java
@@ -2,6 +2,7 @@ package com.github.dwursteisen.threads;
 
 import com.github.dwursteisen.threads.webservice.IWebservice;
 import com.github.dwursteisen.threads.webservice.Quotes;
+import com.github.dwursteisen.threads.webservice.Twitter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,60 +18,99 @@ public class MonProgramme {
 
     public static void main(final String... args) throws InterruptedException {
 
-        final Quotes quotes = new Quotes();
+        final IWebservice quotes = new Quotes();
+        final IWebservice twitter = new Twitter();
 
-        final Thread checkQuotesService = new Thread() {
-
-            @Override
-            public void run() {
-                quotes.demarrage();
-                boolean running = true;
-                while (running) {
-                    try {
-                        System.out.println(quotes.interrogation());
-                        sleep(100);
-                    } catch (InterruptedException e) {
-                        quotes.arret();
-                        running = false;
-                    }
-                }
-            }
-        };
-
-        Thread interruptor = new Thread() {
-            @Override
-            public void run() {
-                checkQuotesService.interrupt();
-            }
-        };
+        final Thread checkQuotesService = new InterrogationWebservice(quotes);
+        final Thread checkTwitterService = new InterrogationWebservice(twitter);
 
         checkQuotesService.start();
+        checkTwitterService.start();
 
-        Runtime.getRuntime().addShutdownHook(interruptor);
-        Runtime.getRuntime().addShutdownHook(new Thread(new ShutdownHock(Arrays.asList(quotes))));
+        Thread webserviceVerificator = new WebserviceVerificator(Arrays.asList(quotes, twitter));
+        Runtime.getRuntime().addShutdownHook(webserviceVerificator);
 
     }
 
 
-    private static class ShutdownHock implements Runnable {
+    private static class WebserviceVerificator extends Thread {
 
-        private Collection<IWebservice> threads;
+        private List<String> threadImportant = Arrays.asList(
+                "Monitor Ctrl-Break",
+                "DestroyJavaVM",
+                "verificator" // ça, c'est moi !
+        );
 
-        public ShutdownHock() {
+        private Collection<IWebservice> webservices;
+
+        public WebserviceVerificator() {
             this(new ArrayList<IWebservice>());
         }
 
-        public ShutdownHock(final List<? extends IWebservice> threads) {
-            this.threads = new ArrayList<IWebservice>(threads);
+        public WebserviceVerificator(final List<? extends IWebservice> webservices) {
+            super("verificator");
+            this.webservices = new ArrayList<IWebservice>(webservices);
+        }
+
+
+        private void interruptionDesThreads() {
+            System.out.println("Interruption de tous les threads...");
+            ThreadGroup rootGroup = Thread.currentThread().getThreadGroup();
+
+            System.out.println("groupname : "+rootGroup.getName());
+
+            Thread[] threads = new Thread[rootGroup.activeCount()];
+            while (rootGroup.enumerate(threads, true) == threads.length) {
+                threads = new Thread[threads.length * 2];
+            }
+
+            for (Thread thread : threads) {
+                if(thread == null || threadImportant.contains(thread.getName())) {
+                    continue;
+                }
+
+                System.out.println("Interruption du thread "+thread.getName());
+                thread.interrupt();
+            }
         }
 
         public void run() {
-            for (IWebservice thread : threads) {
-                if (thread.isVivant()) {
-                    System.err.println("Hey ! Tu as oublié de faire un stop sur le service " + thread.nomService());
+            interruptionDesThreads();
+            verificationDesServices();
+            System.out.println("Fin de l'application...");
+        }
+
+        private void verificationDesServices() {
+            for (IWebservice webservice : webservices) {
+                if (webservice.isVivant()) {
+                    System.err.println("Hey ! Tu as oublié de faire un stop sur le service " + webservice.nomService());
                 }
             }
-            System.out.println("Fin de l'application...");
+        }
+    }
+
+    private static class InterrogationWebservice extends Thread {
+
+        private final IWebservice webservice;
+
+        public InterrogationWebservice(IWebservice webservice) {
+            super("Interrogation du service " + webservice.nomService());
+            this.webservice = webservice;
+        }
+
+        @Override
+        public void run() {
+            webservice.demarrage();
+            boolean running = true;
+            while (running) {
+                try {
+                    System.out.println(webservice.interrogation());
+                    sleep(100);
+                } catch (InterruptedException e) {
+                    webservice.arret();
+                    running = false;
+                }
+            }
         }
     }
 }
