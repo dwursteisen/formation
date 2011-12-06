20c8e491d4a95c0b2031126bcf2ec967ff7bca05
dwursteisen <david.wursteisen+git@gmail.com>
2011-12-06 20:21:38 +0100

    solution pour la formation TDD

--- 07 - TDD/src/my/test/implementation/MonImplementationDeBase.java
+++ 07 - TDD/src/my/test/implementation/MonImplementationDeBase.java
@@ -9,45 +9,77 @@ import java.util.Map;
 
 public class MonImplementationDeBase implements MonAnnuaire {
 
+    private Map<Personne, String> dictionnaire = new HashMap<Personne, String>();
 
+    public void ajoutPersonne(String nom, String prenom, String telephone) {
+        dictionnaire.put(new Personne(nom, prenom), telephone);
+    }
+
+    public int taille() {
+        return dictionnaire.size();
+    }
+
+    public boolean suppressionPersonne(String nom, String prenom) {
+        return dictionnaire.remove(new Personne(nom, prenom)) != null;
+    }
+
+    public String telephone(String nom, String prenom) {
+        return dictionnaire.get(new Personne(nom, prenom));
+    }
+
+    public Collection<String> tousLesNuméros() {
+        return new HashSet<String>(dictionnaire.values());
+    }
+
+    public String nomAssociéAuTéléphone(String téléphone) {
+        return trouvePersonne(téléphone).nom;
+    }
+
+    public String prenomAssociéAuTéléphone(String téléphone) {
+        return trouvePersonne(téléphone).prenom;
+    }
+
+    private Personne trouvePersonne(final String téléphone) {
+        for (Map.Entry<Personne, String> entry : dictionnaire.entrySet()) {
+            if (téléphone.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    private class Personne {
+        final private String nom;
+        final private String prenom;
+
+        private Personne(String nom, String prenom) {
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Personne personne = (Personne) o;
+
+            if (nom != null ? !nom.equals(personne.nom) : personne.nom != null) return false;
+            if (prenom != null ? !prenom.equals(personne.prenom) : personne.prenom != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = nom != null ? nom.hashCode() : 0;
+            result = 31 * result + (prenom != null ? prenom.hashCode() : 0);
+            return result;
+        }
+    }
 
 	@Override
 	public String nomImplementation() {
 		return "MonImplementationDeBase";
 	}
-
-    @Override
-    public void ajoutPersonne(String nom, String prénom, String téléphone) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public int taille() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public boolean suppressionPersonne(String nom, String prénom) {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public String telephone(String nom, String prénom) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public Collection<String> tousLesNuméros() {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public String nomAssociéAuTéléphone(String téléphone) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
-
-    @Override
-    public String prenomAssociéAuTéléphone(String téléphone) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
-    }
 }
--- 07 - TDD/test/my/test/implementation/MonImplementationDeBaseTest.java
+++ 07 - TDD/test/my/test/implementation/MonImplementationDeBaseTest.java
@@ -19,12 +19,63 @@ public class MonImplementationDeBaseTest {
         annuaire = new MonImplementationDeBase();
     }
 
+
+    @Test
+    public void testTaille() throws Exception {
+        assertEquals(0, annuaire.taille());
+        annuaire.ajoutPersonne("nom", "prenom", "0231445422");
+        assertEquals(1, annuaire.taille());
+        annuaire.suppressionPersonne("nom", "prenom");
+        assertEquals(0, annuaire.taille());
+    }
+
     @Test
     public void testAjoutPersonne() throws Exception {
         annuaire.ajoutPersonne("nom", "prenom", "0231445422");
         assertEquals(1, annuaire.taille());
         annuaire.ajoutPersonne("nom", "prenom", "0231445422");
         assertEquals(1, annuaire.taille());
+    }
+
+    @Test
+    public void testSuppressionPersonne() throws Exception {
+        assertFalse(annuaire.suppressionPersonne("nom", "prenom"));
+        annuaire.ajoutPersonne("nom", "prenom", "0231445422");
+        assertTrue(annuaire.suppressionPersonne("nom", "prenom"));
+    }
+
+    @Test
+    public void testTelephone() throws Exception {
+        assertNull(annuaire.telephone("nom", "prenom"));
+        annuaire.ajoutPersonne("nom", "prenom", "0231445422");
+        assertEquals("0231445422", annuaire.telephone("nom", "prenom"));
+    }
+
+    @Test
+    public void testTousLesNumeros() throws Exception {
+        annuaire.ajoutPersonne("nom1", "prenom1", "0231445422");
+        annuaire.ajoutPersonne("nom2", "prenom2", "0231445422");
+        annuaire.ajoutPersonne("nom3", "prenom3", "0211111111");
+        annuaire.ajoutPersonne("nom4", "prenom4", "0222222222");
+
+        Collection<String> numeros = annuaire.tousLesNuméros();
+
+        assertEquals(3, numeros.size());
+    }
+
+
+    @Test
+    public void testNomAssociéAuTéléphone() {
+        annuaire.ajoutPersonne("nom1", "prenom1", "0231445422");
+
+        assertEquals("nom1", annuaire.nomAssociéAuTéléphone("0231445422"));
+    }
+
+    @Test
+    public void testPrenomAssociéAuTéléphone() {
+        annuaire.ajoutPersonne("nom1", "prenom1", "0231445422");
+
+        assertEquals("prenom1", annuaire.prenomAssociéAuTéléphone("0231445422"));
     }
 
 
